4c75b0b905d800c934514d9e76c32db19a260831
RicardVG <ricardvinolas10@gmail.com>
2022-03-12 13:25:02 +0100

    Single Fragment Activity + API 28

--- app/src/main/java/com/androidpprog2/crimenes/SingleFragmentActivity.java
+++ app/src/main/java/com/androidpprog2/crimenes/SingleFragmentActivity.java
@@ -2,14 +2,17 @@ package com.androidpprog2.crimenes;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-public class CrimeActivity extends AppCompatActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
 
@@ -17,10 +20,11 @@ public class CrimeActivity extends AppCompatActivity {
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
         if (fragment == null){
-            fragment = new CrimeFragment();
+            fragment = createFragment();
             fm.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit();
         }
     }
 }
+
